075fd984abd5c885a4320b6254ad805dc897c595
ulikemywa1 <calium@outlook.com>
2024-04-10 10:53:41 +0300

    feat: implement queuPromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -138,8 +138,19 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = '';
+  let sequence = Promise.resolve();
+
+  promises.forEach((promise) => {
+    sequence = sequence
+      .then(() => promise)
+      .then(function concat(value) {
+        result += value;
+      });
+  });
+
+  return sequence.then(() => result);
 }
 
 module.exports = {
